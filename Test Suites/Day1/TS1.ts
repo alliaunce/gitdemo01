25f06ccf905492a026eefa3d5f1f32c6cbe4eefb
math8624 <60137945+math8624@users.noreply.github.com>
2020-02-03 14:57:06 +0530

    Commit 1

---
+++ Test Suites/Day1/TS1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>db5611d0-a0d9-46ed-a685-8513559d60fc</testSuiteGuid>
+   <testCaseLink>
+      <guid>4468880e-e50e-4e3d-8547-abe5fb58e11a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day1/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
